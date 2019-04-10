7b31ff8d8717e37eff1ab99d8757bd856e2d84f9
Derek Gomez <49483169+22gomede@users.noreply.github.com>
2019-04-10 12:40:41 -0400

    Adding hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Derek Gomez
+# 04/10/19
+
+#######################
+#     Hello World     #
+#                     #
+#  Our First Python   #
+#    Application      #
+#######################
+
+# print "Hello, World" to the screen.
+print("Hello, World")
